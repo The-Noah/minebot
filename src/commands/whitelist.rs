b083be8d3320920652e9f3c274bd10d11f14960b
The Noah <thenoahbz@gmail.com>
2024-06-21 14:51:24 -0500

    chore: make whitelist command locked to admins

--- src/commands/whitelist.rs
+++ src/commands/whitelist.rs
@@ -1,4 +1,4 @@
-use serenity::all::CreateCommandOption;
+use serenity::all::{CreateCommandOption, Permissions};
 use serenity::builder::CreateCommand;
 use serenity::model::application::{ResolvedOption, ResolvedValue};
 
@@ -44,6 +44,7 @@ pub async fn run<'a>(options: &'a [ResolvedOption<'a>]) -> String {
 pub fn register() -> CreateCommand {
   CreateCommand::new("whitelist")
     .description("Manage the whitelist")
+    .default_member_permissions(Permissions::ADMINISTRATOR)
     .add_option(
       CreateCommandOption::new(serenity::all::CommandOptionType::SubCommand, "add", "Add a player to the whitelist")
         .add_sub_option(CreateCommandOption::new(serenity::all::CommandOptionType::String, "username", "Minecraft username").required(true)),
